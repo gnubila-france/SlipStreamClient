85119fb01f2d1211c422ddabf014cd18092aff4f
Jerome Revillard <jrevillard@maatg.fr>
2014-01-29 11:35:51 +0100

    Test SSLv3 connection first

--- client/src/external/httplib2/__init__.py
+++ client/src/external/httplib2/__init__.py
@@ -73,11 +73,16 @@ try:
             cert_reqs = ssl.CERT_NONE
         else:
             cert_reqs = ssl.CERT_REQUIRED
-        # We should be specifying SSL version 3 or TLS v1, but the ssl module
-        # doesn't expose the necessary knobs. So we need to go with the default
-        # of SSLv23.
-        return ssl.wrap_socket(sock, keyfile=key_file, certfile=cert_file,
-                               cert_reqs=cert_reqs, ca_certs=ca_certs)
+	# Our fix for sites the only accepts SSL3
+        try:
+            # Trying SSLv3 first
+            return ssl.wrap_socket(sock, keyfile=key_file, certfile=cert_file,
+                                       cert_reqs=cert_reqs, ca_certs=ca_certs,
+                                       ssl_version=ssl.PROTOCOL_SSLv3)
+        except ssl.SSLError, e:
+            return ssl.wrap_socket(sock, keyfile=key_file, certfile=cert_file,
+                                       cert_reqs=cert_reqs, ca_certs=ca_certs,
+                                       ssl_version=ssl.PROTOCOL_SSLv23)
 except (AttributeError, ImportError):
     ssl_SSLError = None
     def _ssl_wrap_socket(sock, key_file, cert_file,
